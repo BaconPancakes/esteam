45c3960891aee32d571bba95fb25cff91b720f62
BaconPancakes <me2myself3andi@gmail.com>
2014-08-30 18:30:47 -0400

    Lookup model complete for Sat.water
    Full functionality for lookup of all thermo properties of saturated
    water.

--- app/models/steam_table.rb
+++ app/models/steam_table.rb
@@ -3,7 +3,7 @@ require 'bsearch'
 
 class SteamTable
   # define which thermodynamic property each column in the steam table represents
-  PROPERTY= {:temperature => 0, :pressure => 1, :v_f => 2, :v_g => 3, :u_f => 4, :u_g => 5}
+  PROPERTY= {temperature: 0, pressure: 1, v_f: 2, v_g: 3, u_f: 4, u_g: 5, h_f: 6, h_g: 7, s_f: 8, s_g: 9}
 
   # initialize steam table
   def initialize
@@ -13,8 +13,13 @@ class SteamTable
 # lookup method
   def lookup(rawparams)
     params = getparams(rawparams)
+
+    # if the user has selected 'saturated'
     if rawparams[:saturated]
       return satlookup(params)
+
+    else
+
     end
   end
 
@@ -109,9 +114,9 @@ class SteamTable
           return quality_from_pressure(pressure, property.to_s, params[property].to_f)
         end
       end
-    else
-      return ''
     end
+    return ''
+  end
 
 =begin
          # We should have two specific properties. Otherwise, quality calculation is not possible.
@@ -124,34 +129,55 @@ class SteamTable
         end
 =end
 
-  end
 
-  # for a saturated substance, given temperature and quality, look up pressure
-  # returns hash of all other properties
+# for a saturated substance, given temperature and quality, look up pressure
+# returns hash of all properties
   def satlookup_from_pressure(pressure, quality)
     index = search_column('pressure', pressure)
     temperature = interpolate(index, 'pressure', pressure, 'temperature')
+
     v_f = interpolate(index, 'pressure', pressure, 'v_f')
     v_g = interpolate(index, 'pressure', pressure, 'v_g')
     u_f = interpolate(index, 'pressure', pressure, 'u_f')
     u_g = interpolate(index, 'pressure', pressure, 'u_g')
+    h_f = interpolate(index, 'pressure', pressure, 'h_f')
+    h_g = interpolate(index, 'pressure', pressure, 'h_g')
+    s_f = interpolate(index, 'pressure', pressure, 's_f')
+    s_g = interpolate(index, 'pressure', pressure, 's_g')
+
     specific_volume = specifics_from_quality(quality, v_f, v_g)
     specific_energy = specifics_from_quality(quality, u_f, u_g)
+    specific_enthalpy = specifics_from_quality(quality, h_f, h_g)
+    specific_entropy = specifics_from_quality(quality, s_f, s_g)
+
     return {pressure: pressure, temperature: temperature, specific_volume: specific_volume,
-            specific_energy: specific_energy, quality: quality}
+            specific_energy: specific_energy, specific_enthalpy: specific_enthalpy,
+            specific_entropy: specific_entropy, quality: quality}
   end
 
+  # for a saturated substance, given pressure and quality, look up temperature
+  # returns hash of all properties
   def satlookup_from_temperature(temperature, quality)
     index = search_column('temperature', temperature)
     pressure = interpolate(index, 'temperature', temperature, 'pressure')
+
     v_f = interpolate(index, 'temperature', temperature, 'v_f')
     v_g = interpolate(index, 'temperature', temperature, 'v_g')
     u_f = interpolate(index, 'temperature', temperature, 'u_f')
     u_g = interpolate(index, 'temperature', temperature, 'u_g')
+    h_f = interpolate(index, 'pressure', temperature, 'h_f')
+    h_g = interpolate(index, 'pressure', temperature, 'h_g')
+    s_f = interpolate(index, 'pressure', temperature, 's_f')
+    s_g = interpolate(index, 'pressure', temperature, 's_g')
+
     specific_volume = specifics_from_quality(quality, v_f, v_g)
     specific_energy = specifics_from_quality(quality, u_f, u_g)
+    specific_enthalpy = specifics_from_quality(quality, h_f, h_g)
+    specific_entropy = specifics_from_quality(quality, s_f, s_g)
+
     return {pressure: pressure, temperature: temperature, specific_volume: specific_volume,
-            specific_energy: specific_energy, quality: quality}
+            specific_energy: specific_energy, specific_enthalpy: specific_enthalpy,
+            specific_entropy: specific_entropy, quality: quality}
   end
 
 
@@ -183,8 +209,7 @@ class SteamTable
   end
 
 # given the quality, and the values of desired property
-# as a saturated liquid or saturated gas, calculate the desired
-# property (specific volume, energy for now)
+# as a saturated liquid or saturated gas, calculate the desired property
   def specifics_from_quality(quality, y_f, y_g)
     result = (quality/100) * (y_g-y_f) + y_f
     return result.round(3)
@@ -237,7 +262,7 @@ class SteamTable
     y_f = interpolate(index, 'temperature', temperature_value, y_f)
     y_g = interpolate(index, 'temperature', temperature_value, y_g)
 
-    quality = 100* (value - y_f)/(y_g-y_f)
+    quality = 100 * (value - y_f)/(y_g-y_f)
     if quality > 100
       quality = 100
     end
